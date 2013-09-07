960b249c21093172fc6840689ee20c1727d4ac0c
PureIso <olaegby@live.com>
2013-09-07 09:56:32 +0100

    Started Neural Networks
    Started Neural Networks

---
+++ VLANeuralNetwork/EntryPoint.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VLAControlLib;
+using VLAPluginLib;
+
+namespace VLANeuralNetwork
+{
+    public class EntryPoint : VLAPlugin
+    {
+        public static DefaultPage HostDefaultPage; //Get the Host DefaultPage user Control
+        public static LogTextDelegate HostLogger; //Get the Host Log Box
+        public static ProgressBarDelegate HostProgressBar; //Get the Host ProgressBar
+
+        //Loaded when it's discovered by the plug-in service and all base variables are set
+        public EntryPoint()
+        {
+            base.IllustrationName = "Linked List Illustration";
+            base.IllustrationVersion = "1.0.0.0";
+            base.IllustrationSupportedLanguages = new List<string>(new[] { "English" });
+            base.PluginType = PluginType.Illustration;
+            base.IllustrationAuthor = "Ola";
+            base.IllustrationDescription = "Linked List Data Structure Illustration";
+            base.IllustrationModuleName = new List<string>(
+                new[]
+                    {
+                        "Data Structures",
+                    });
+        }
+
+        public override void IllustrationDisplay()
+        {
+            HostDefaultPage = base.HDefaultPage;
+            HostLogger = base.HLog;
+            HostProgressBar = base.HProgressBar;
+            HostDefaultPage.Switch = new Main();
+        }
+    }
+}
